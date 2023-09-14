8d26a20b014e112dbad6f7f47e1bad20ca8bd0e7
Noob Master669 <noobmaster669@protonmail.com>
2023-09-14 17:32:11 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- utilities/utils_metadata.py
+++ utilities/utils_metadata.py
@@ -547,11 +547,7 @@ def _fill_tmdb_metadata_to_torrent_info(torrent_info, tmdb_response):
         if "original_language" in tmdb_response
         else ""
     )
-    tmdb_metadata["spoken_languages"] = (
-        tmdb_response["spoken_languages"]
-        if "spoken_languages" in tmdb_response
-        else {}
-    )
+    tmdb_metadata["spoken_languages"] = tmdb_response.get("spoken_languages", {})
 
     tmdb_metadata["genres"] = (
         list(map(lambda genre: genre["name"], tmdb_response["genres"]))
