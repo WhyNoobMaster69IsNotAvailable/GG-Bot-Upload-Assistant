50cb61d55c7312e1a3e19e417ed7ff364a8c58c4
NoobMaster69 <noobmaster669@protonmail.com>
2023-09-29 04:32:18 +0000

    bug fix ptp 10 bit tags

--- modules/custom_actions/ptp_actions.py
+++ modules/custom_actions/ptp_actions.py
@@ -83,7 +83,7 @@ def _get_tags(imdb_tags, tmdb_tags):
 
 
 def fix_10_bit_tag(torrent_info, tracker_settings, __):
-    remaster_title = tracker_settings["remaster_title"]
+    remaster_title = tracker_settings.get("remaster_title", "")
 
     if "10-bit" not in remaster_title:
         return
